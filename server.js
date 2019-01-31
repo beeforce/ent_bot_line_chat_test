4b8a89079fc6cd18ee5c13641235f8787c8d58b3
Buri Paoton <bakeforce75@gmail.com>
2019-01-31 17:12:17 +0700

    [Added] -image map message

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ function handleEvent(event) {
   console.log(event);
   if(event.type === 'message') {
     handleMessageEvent(event);
+    handleImagemapMessageEvent(event);
   }
   else {
     return Promise.resolve(null)
@@ -34,7 +35,56 @@ function handleEvent(event) {
 function handleMessageEvent(event) {
   var msg = {
     type: "text",
-    text: "this is"+ event.message.type
+    text: "this is "+ event.message.type
+  }
+  return client.replyMessage(event.replyToken, msg);
+}
+
+function handleImagemapMessageEvent(event) {
+  var msg = {
+    "type": "imagemap",
+    "baseUrl": "https://example.com/bot/images/rm001",
+    "altText": "This is an imagemap",
+    "baseSize": {
+        "width": 1040,
+        "height": 1040
+    },
+    "video": {
+        "originalContentUrl": "https://example.com/video.mp4",
+        "previewImageUrl": "https://example.com/video_preview.jpg",
+        "area": {
+            "x": 0,
+            "y": 0,
+            "width": 1040,
+            "height": 585
+        },
+        "externalLink": {
+            "linkUri": "https://example.com/see_more.html",
+            "label": "See More"
+        }
+    },
+    "actions": [
+        {
+            "type": "uri",
+            "linkUri": "https://example.com/",
+            "area": {
+                "x": 0,
+                "y": 586,
+                "width": 520,
+                "height": 454
+            }
+        },
+        {
+            "type": "message",
+            "text": "Hello",
+            "area": {
+                "x": 520,
+                "y": 586,
+                "width": 520,
+                "height": 454
+            }
+        }
+    ]
   }
   return client.replyMessage(event.replyToken, msg);
 }
